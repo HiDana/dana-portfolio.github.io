eee6cb7d369e898d58b0bbd9612dff9d59370cb8
Dana <holicdana@gmail.com>
2019-08-12 16:38:45 +0800

    Change npm build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 
@@ -9,27 +9,15 @@ import Project from "./screens/Project/Project";
 
 class App extends Component {
   render() {
-    console.log("process", process);
-
     return (
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Router>
         <div>
           <Header />
-          <Route
-            exact
-            path={`${process.env.PUBLIC_URL}/`}
-            component={Personal}
-          />
-          <Route
-            path={`${process.env.PUBLIC_URL}/portfolio`}
-            component={Portfolio}
-          />
-          <Route
-            path={`${process.env.PUBLIC_URL}/project/:prjName`}
-            component={Project}
-          />
+          <Route exact path="/" component={Personal} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/project/:prjName" component={Project} />
         </div>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
